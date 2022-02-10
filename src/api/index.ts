a7f2e2f19b0575db2a1df2e5d8504a0f98b90d18
Pinto565 <pintoinfant5650@gmail.com>
2022-02-10 23:48:27 +0530

    cronjob to delete files

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express"
 
 import quickMemes from "./quick-memes/quick-memes.route"
 import devTo from "./dev.to/dev.to.route"
+import screenShot from "./screenshot/screenshot.route"
 
 const api: Router = Router()
 
 api.use("/quick-memes", quickMemes)
 api.use("/dev-to", devTo)
+api.use("/screenshot", screenShot)
 
 export default api
---
+++ src/cronjob.ts
@@ -0,0 +1,17 @@
+import cron from "node-cron"
+import * as fs from "fs"
+import * as path from "path"
+
+let directory = "public"
+
+export const cronJob = cron.schedule('0 0 * * *', () => {
+    fs.readdir(directory, (err, files) => {
+        if (err) throw err
+        files.forEach((file) => {
+            fs.unlink(path.join(directory, file), (err) => {
+                if (err) throw err
+            })
+        })
+    })
+});
+
