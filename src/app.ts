af3c22f718f3c10378c012d0a41a3d2df97c5ff1
Pinto565 <pintoinfant5650@gmail.com>
2022-02-10 23:01:11 +0530

    vercel json

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express"
+import express, { Application, Request, Response } from "express"
 import api from "./api"
 
 const app: Application = express()
@@ -7,6 +7,13 @@ const app: Application = express()
 //MiddleWares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req: Request, res: Response) => {
+    return res.json({
+        author: "Pinto Infant",
+        githubUrl: "https://github.com/Pinto565/api-hub"
+    })
+})
 
 app.use("/api", api)
 
